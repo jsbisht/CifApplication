3a2b1c24e70ad820b1f0997f0ce4501460b3f09a
Kunal Sagar <k.sagar@prowareness.nl>
2017-08-17 17:49:53 +0530

    Added address details fields

---
+++ db_connection.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "pro@321",
+database: "cif_test"
+});
+
+con.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");    
+} else {
+    console.log("Error connecting database ... nn");    
+}
+});
+var id = 6;
+var post  = {cid:id, EmployeeName: 'ddd',EmployeeId:id};
+var insQuery = 'insert into candidate SET ?';
+var query = con.query(insQuery ,post, function(err, rows, fields) {
+con.end();
+  if (!err)
+    console.log('The solution is: ', rows, query );
+  else
+    console.log('Error while performing Query.' + err );
+ });
+ 
