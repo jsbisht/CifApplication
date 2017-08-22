da139e2f6261f12d96acf517b4137fa9141b184c
Shwetha Krishnarao <ba.raghu@gmail.com>
2017-08-22 13:55:22 +0530

    UI changes to align UI elements

--- public/app/controllers/homeController.js
+++ public/app/controllers/homeController.js
@@ -1,8 +1,27 @@
 ﻿var homeController = candidateInformation.controller("homeController", ['$scope', 'cifService', '$state', function ($scope, cifService, $state) {
     $scope.candidate = {};
-    angular.copy(cifService.candidate, $scope.candidate);
-    $scope.submitForm = function () {
-        cifService.candidate = $scope.candidate;
-        $state.go('navigation');
+    $scope.navigations = ['personal', 'address', 'education', 'employment', 'reference', 'emergency', 'additional'];
+    $scope.candidate = cifService.candidate;
+    $scope.enableSubmit = true;
+
+    if ($scope.candidate.personalDetails && $scope.candidate.employmentDetails && $scope.candidate.educationDetails &&
+        $scope.candidate.referenceDetails && $scope.candidate.emergencyDetails  && $scope.candidate.address) {
+        $scope.enableSubmit = false;
     }
-}])
+
+    $scope.saveUserDetails = function () {
+        if (window.confirm("Details once save can not be modified. Are you sure you want to submit?")) {
+            //save the user details to database
+            cifService.saveDetails().then(function (response) {
+                // do something with the response
+                $state.go("summary");
+                console.log(response);
+            }, function (error) {
+                // log the error 
+                console.log(error);
+            });
+        } else {
+            return false;
+        }
+    }
+}]);
